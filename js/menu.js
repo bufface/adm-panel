deed9e8461af17c09cbf6bc9f860a978b11fb89b
Cristian Dell <bufface@gmail.com>
2016-05-20 12:03:13 -0300

    Initial commit

---
+++ js/menu.js
@@ -0,0 +1,12 @@
+var $hamburger = $(".hamburger");
+$hamburger.on("click", function(e) {
+  $hamburger.toggleClass("is-active");
+  
+  if ($hamburger.is('.is-active')) {
+
+  }
+  else {
+
+  }
+  
+});
