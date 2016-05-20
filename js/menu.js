a7bee829309d020df74bf517813b6e76f49084de
Cristian Dell <bufface@gmail.com>
2016-05-20 13:48:50 -0300

    Fix 'hamburgers' plugin behavior

--- js/menu.js
+++ js/menu.js
@@ -1,12 +1,15 @@
 var $hamburger = $(".hamburger");
 $hamburger.on("click", function(e) {
-  $hamburger.toggleClass("is-active");
+
+	$hamburger.toggleClass("is-active");
   
   if ($hamburger.is('.is-active')) {
-
+  	$('.left-menu').parent().removeClass('hide')
+  	$('.content').parent().removeClass('col-md-12').addClass('col-md-10');
   }
   else {
-
+  	$('.left-menu').parent().addClass('hide')
+  	$('.content').parent().removeClass('col-md-10').addClass('col-md-12');
   }
   
 });
