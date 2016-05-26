bbb74b0cc33d30722cd39f9571195e064c9f9e36
Cristian Dell <bufface@gmail.com>
2016-05-26 09:53:21 -0300

    Removed 'Hamburger' menu icon and JS. Added a new one named 'Animated Menu Icon', link: http://codepen.io/egrucza/pen/LNKvwY

---
+++ js/menu/menu.js
@@ -0,0 +1,25 @@
+'use strict';
+function _classCallCheck(instance, Constructor) {
+    if (!(instance instanceof Constructor)) {
+        throw new TypeError('Cannot call a class as a function');
+    }
+}
+var MenuHandler = function () {
+    function MenuHandler() {
+        _classCallCheck(this, MenuHandler);
+        this.menuTrigger = $('#menu-trigger');
+        this.menu = $('.menu');
+        this.toggleMenu();
+    }
+    MenuHandler.prototype.toggleMenu = function toggleMenu() {
+        var _this = this;
+        this.menuTrigger.on('click', function () {
+            _this.menuTrigger.toggleClass('is-open');
+            _this.menu.toggleClass('is-open');
+        });
+    };
+    return MenuHandler;
+}();
+$(document).ready(function ($) {
+    var menuHandler = new MenuHandler('.menu');
+});
